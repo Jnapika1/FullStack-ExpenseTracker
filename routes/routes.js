c56ce731a35da8d7791688cd63a0d37db399f9d9
Jnapika1 <jnapika.aish@gmail.com>
2023-06-01 15:01:44 +0530

    added login frontend

--- routes/routes.js
+++ routes/routes.js
@@ -7,4 +7,8 @@ const userController = require('../controllers/signup');
 
 router.post('/user/signup', userController.postSignupUser);
 
+router.post('/user/login', (req, res, next)=>{
+    console.log(req);
+})
+
 module.exports=router;
