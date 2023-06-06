2e539ee735430069d148d7eaeea3c4b57b75afc5
Jnapika1 <jnapika.aish@gmail.com>
2023-06-06 21:01:42 +0530

    frontend report

--- controllers/password.js
+++ controllers/password.js
@@ -21,27 +21,22 @@ const forgotpassword = async (req, res) => {
             sgMail.setApiKey(process.env.SENGRID_API_KEY)
 
             const msg = {
-                to: email, // Change to your recipient
-                from: 'aishwarya200499@gmail.com', // Change to your verified sender
-                subject: 'Sending with SendGrid is Fun',
-                text: 'and easy to do anywhere, even with Node.js',
-                html: `<a href="http://localhost:8000/password/resetpassword/${id}">Reset password</a>`
+                to: email,
+                from: 'aishwarya200499@gmail.com',
+                subject: 'RESET Password',
+                text: 'This is your link to reset password',
+                html: `<p>Please click on the given link to reset password</p><a href="http://localhost:8000/password/resetpassword/${id}">Reset password</a>`
             }
 
             sgMail
             .send(msg)
             .then((response) => {
-
-                // console.log(response[0].statusCode)
-                // console.log(response[0].headers)
-                return res.status(response[0].statusCode).json({message: 'Link to reset password sent to your mail ', sucess: true})
+                return res.status(response[0].statusCode).json({message: 'Link to reset password sent to your mail ', success: true})
 
             })
             .catch((error) => {
                 throw new Error(error);
             })
-
-            //send mail
         }else {
             throw new Error('User doesnt exist')
         }
@@ -85,10 +80,7 @@ const updatepassword = (req, res) => {
         const { resetpasswordid } = req.params;
         Forgotpassword.findOne({ where : { id: resetpasswordid }}).then(resetpasswordrequest => {
             User.findOne({where: { id : resetpasswordrequest.userId}}).then(user => {
-                // console.log('userDetails', user)
                 if(user) {
-                    //encrypt the password
-
                     const saltRounds = 10;
                     bcrypt.genSalt(saltRounds, function(err, salt) {
                         if(err){
@@ -96,7 +88,6 @@ const updatepassword = (req, res) => {
                             throw new Error(err);
                         }
                         bcrypt.hash(newpassword, salt, function(err, hash) {
-                            // Store hash in your password DB.
                             if(err){
                                 console.log(err);
                                 throw new Error(err);
